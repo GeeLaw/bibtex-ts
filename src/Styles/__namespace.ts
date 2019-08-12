575e6d76c5fe143a13de0e8bcc26e2eb5015d806
Gee Law <geelaw@outlook.com>
2019-08-12 08:15:09 -0700

    Start building BST utilities.

---
+++ src/Styles/__namespace.ts
@@ -0,0 +1,13 @@
+ExportBibTeX.Styles = (function (ns: any): any
+{
+ns.Alpha = Styles_Alpha;
+
+return ns;
+})(Helper.NewEmptyObject());
+
+ExportBibTeX._Privates.Styles = (function (ns: any): any
+{
+ns.Alpha = Styles_Alpha;
+
+return ns;
+})(Helper.NewEmptyObject());
